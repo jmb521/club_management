7833050009f6bf73bbf3feb8db435e2254e68992
Jennifer Pazos <jmp521@gmail.com>
2018-07-27 01:28:31 -0500

    updated user relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_one :profile
+  has_one :profile, :autosave => true
+  has_many :kids
+  has_one :family, :autosave => true
+  has_one :membership_infos, :autosave => true
+
+  before_create :build_membership_infos
+  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
