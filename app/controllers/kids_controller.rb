0f8aa11444860c69e348aac9ac8b807d0c4c9504
Jennifer Pazos <jmp521@gmail.com>
2018-07-30 00:42:01 -0500

    kids controller

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,2 +1,33 @@
 class KidsController < ApplicationController
+
+  def index
+    @kids = Kid.find_by(:profile_id => params[:profile_id])
+  end
+
+  def show
+
+  end
+
+  def new
+    @kid = Kid.new
+  end
+
+  def create
+    @kid = Kid.create(params[:kid_params])
+    redirect profile_path(@kid.profile_id)
+  end
+
+  def edit
+    @kid = Kid.find(params[:kid_params])
+  end
+
+  def update
+
+  end
+
+  private
+
+  def kid_params
+    params.require(:kid).permit(:kid_name, :kid_birthday, :profile_id)
+  end
 end
