d3ad0a5a343d0a675bf95c3ae317594472e3791d
Jennifer Pazos <jmp521@gmail.com>
2018-08-11 01:00:54 -0500

    updated method that control redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,21 @@ class ApplicationController < ActionController::Base
   def home
     render react_component: 'Hello', props: { name: 'Jen' }
   end
+
+  def user_home
+    render user_home_path
+  end
+ # protected
+   def after_sign_in_path_for(resource)
+    #  sign_in_url = new_user_session_url
+     if request.referer == sign_in_url
+       super
+     else
+       stored_location_for(resource) || request.referer || root_path
+     end
+   end
+
+  def after_sign_in_path_for(resource)
+    user_home_path
+  end
 end
