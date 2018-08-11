ff8de6e7d51d3aeb82c7345ffc3085eb2d11cdbd
Jennifer Pazos <jmp521@gmail.com>
2018-08-11 01:01:34 -0500

    added crud methods

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,2 +1,23 @@
 class FamiliesController < ApplicationController
+
+  def show
+    @family = Family.find(params[:id])
+
+  end
+
+  def edit
+    @family = Family.find(params[:id])
+  end
+
+  def update
+    @family = Family.find(params[:id])
+    @family.update(family_params)
+    redirect_to family_path(@family)
+  end
+
+  private
+
+  def family_params
+    params.permit(:families).require(:user_birthday, :spouse, :spouse_birthday, :profile_id)
+  end
 end
