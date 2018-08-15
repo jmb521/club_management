d9b8a116b7339f6a5ac06cbc5de23aa87c7b65f7
Jennifer Pazos <jmp521@gmail.com>
2018-08-15 18:55:02 -0500

    working on integrating react component

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,19 @@
 class AdminController < ApplicationController
-  before_action :authenticate!
-  
+  before_action :authenticate_user!
+
+
+    def dashboard
+      #shows all admin access
+    end
+
+    def new_member
+      #use for approving new members
+      @users = User.all.where(:role => "applicant")
+      render react_component: 'ApproveMember', props: { name: 'Jen' }
+
+    end
+
+    def approve_member
+      @users = User.all.where(:role => "applicant")
+    end
 end
